e1bc80a647e1148b8dcdeae3fddbda58c75c7085
Gustave Abel Michel III <gus3@michelfamily.org>
2014-04-07 00:27:49 -0500

    Added Splash Screen for use when game starts up.

--- src/helixprophets/Main.java
+++ src/helixprophets/Main.java
@@ -1,9 +1,137 @@
 package helixprophets;
 
-public class Main {
+import static org.lwjgl.opengl.GL11.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.lwjgl.input.Mouse;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.*;
+import org.lwjgl.*;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+
+
+public class Main {
+	private static volatile boolean isSplash = true;
+	
 	public static void main(String[] args) {
+		Main game = new Main();
+		String title = "Game Title";
+		game.initDisplay(title, 800, 600);
+		game.splash();
+		game.open();
+		game.play();
+		game.close();
+	}
+	
+	/**
+	 * Initializes the Display and OpenGL
+	 * @param title Window Title
+	 * @param width Window Width
+	 * @param height Window Height
+	 */
+	public void initDisplay(String title, int width, int height) {
+		try {
+			Display.setDisplayMode(new DisplayMode(width, height));
+			Display.setTitle(title);
+			Display.create();
+			Display.setVSyncEnabled(true);
+			
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		GL11.glEnable(GL11.GL_TEXTURE_2D);              
+		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);         
+		// enable alpha blending
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		 
+		GL11.glViewport(0,0,width,height);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		 
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, width, height, 0, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+	}
+	
+	/**
+	 * Displays a Splash Screen for a minimum of one second while game loads
+	 */
+	public void splash() {
+		//Init and bind image as texture
+		Texture splashTexture = null;
+		try {
+			splashTexture = TextureLoader.getTexture("PNG", new FileInputStream(new File("res/splash.png")));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		if(!splashTexture.equals(null)) {
+			System.out.println("Binding");
+			splashTexture.bind(); // or GL11.glBind(texture.getTextureID());
+		} else {
+			Color.black.bind();
+		}
+		//Render Image centered on the screen
+		if(!splashTexture.equals(null)) {
+			GL11.glBegin(GL11.GL_QUADS);
+				GL11.glTexCoord2f(0,0);
+				GL11.glVertex2f((Display.getWidth()/2)-(splashTexture.getTextureWidth()/2), (Display.getHeight()/2)-(splashTexture.getTextureHeight()/2));
+				GL11.glTexCoord2f(1,0);
+				GL11.glVertex2f((Display.getWidth()/2)+(splashTexture.getTextureWidth()/2), (Display.getHeight()/2)-(splashTexture.getTextureHeight()/2));
+				GL11.glTexCoord2f(1,1);
+				GL11.glVertex2f((Display.getWidth()/2)+(splashTexture.getTextureWidth()/2), (Display.getHeight()/2)+(splashTexture.getTextureHeight()/2));
+				GL11.glTexCoord2f(0,1);
+				GL11.glVertex2f((Display.getWidth()/2)-(splashTexture.getTextureWidth()/2), (Display.getHeight()/2)+(splashTexture.getTextureHeight()/2));
+			GL11.glEnd();
+		}
+		Display.update();
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} finally {
+					isSplash = false;
+				}
+			}
+		}).start();
+	}
+	
+	/**
+	 * Initializes Game Setup
+	 */
+	public void open() {
+		
+		while(true);
+	}
+	
+	/**
+	 * Main 
+	 */
+	public void play() {
 		
 	}
-
+	
+	/**
+	 * Closes Game
+	 */
+	public void close() {
+		Display.destroy();
+	}
 }
