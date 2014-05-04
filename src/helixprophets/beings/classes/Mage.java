9a5aa1e52b04e3fe270b065e1175ce52d6e56780
Travis67352 <travis67352@gmail.com>
2014-05-03 19:23:35 -0500

    Character Descriptions
    Supposedly my other upload only commited the fighter class and not the
    mage/rogue clsses

--- src/helixprophets/beings/classes/Mage.java
+++ src/helixprophets/beings/classes/Mage.java
@@ -86,8 +86,51 @@ new Thread(new Runnable() {
 		return 0;
 	}
 
-	public void displayFighter(){
+	/*public void displayFighter(){
 	
-	}
+	}*/
+	
 
+	public void fight()
+	{
+		//Switch to magicness graphic
+		// Read in position of cursor
+		//If the mouse was clicked, create a box entity with gravity physics at that location
+		//if the mouse was held down for half a second or more, create a platform entity
+		//with gravity physics at that location
+		//Start a timer where another object cannot be created until the object disappears (3 sec)
+	
+		/*What the code would look like, not including any classes not implemented yet
+		 * drawTexture(fightTextures[0]);//magic casting stance
+		 * Point cursor= mouse.getLocation(); 
+		 * if(cursor==valid) //valid would be determined by whether or not that space is occupied by a platform or something
+		 *{
+		 *drawTexture(moveTextures[0]); //Standby
+		 *  if(mouseClickDuration<500)
+		 *  {
+		 *    Entity<Box> box= new Entity<Box>(cursor.getX(), cursor.getY());
+		 *    delay(3000); //Box lasts for 3 seconds;
+		 *    box.remove(); //box disappears, a new entity can be made.
+		 *  }
+		 *  else
+		 *  {
+		 *    Entity<Platform> platform= new Entity<Platform>(cursor.getX(), cursor.getY());
+		 *    delay(3000); //Platform lasts for 3 seconds;
+		 *    platform.remove(); //platform disappears, a new entity can be made.
+		 *  }
+		 *}
+		 *drawTexture(moveTextures[0]); //Standby
+		 */
+
+	}
 }
+/* The mage should share the highest jump height with the rogue but should have the lowest 
+ * movespeed. His fight() should create a box or a platform at the mouse position,
+ * which should both be affected by gravity. The box/platform should last for three or so seconds
+ * and then disappear, and fight() should be inaccessible if a box/platform has been created 
+ * within the last three or so seconds. Medium crawl speed
+ * 
+ * He can also see invisible secrets that other classes cant see. These secrets will be denoted
+ *  by a flashy glowy circle around the secret's location, so the secret item or door is still
+ *  accessible to any class if they knew beforehand where it was.
+ */
